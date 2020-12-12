e13a4a690bc6ffa466c8916a431d70fa8a75c465
Elithabeth Golub <golubliza77@gmail.com>
2020-12-12 03:58:26 +0300

    upgrade

---
+++ database_creation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm db.sqlite3
+python3 manage.py makemigrations
+python3 manage.py migrate --run-syncdb
+python3 manage.py createsuperuser
+python3 functions.py
+python3 manage.py loaddata items_final.json
